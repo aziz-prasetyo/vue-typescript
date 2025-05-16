916640421c35f6394f46b1d9cf65f2d87be3a95a
Muhammad Aziz Prasetyo <azizprasetyo100901@gmail.com>
2025-05-16 23:14:20 +0700

    chore: format code for consistency and readability

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,15 +1,14 @@
 <script setup lang="ts">
-
 </script>
 
 <template>
-  <h1 class="hello">Hello World!</h1>
+	<h1 class="hello">Hello World!</h1>
 </template>
 
 <style scoped>
 .hello {
-  text-align: center;
-  margin: 60px 0;
-  color: aquamarine;
+	text-align: center;
+	margin: 60px 0;
+	color: aquamarine;
 }
 </style>
